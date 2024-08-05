539ad02f01c3ba093fa80c00db39ca530f87cf84
Jongwook (John) Whangbo <jwwhangbo@gmail.com>
2024-08-05 13:46:31 +0900

    altered next config: outputFileTracingIncludes

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    outputFileTracingIncludes: {
+      "/": ["./public/**/*"],
+    },
+  },
 };
 
 export default nextConfig;
